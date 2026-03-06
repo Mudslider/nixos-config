bd84b7a7aafca71c02217c94dc04345646256b3e
Philip <philip@home.lan>
2026-03-06 21:46:14 +0100

    remove custom DATABASE_URL from vaultwarden

--- modules/server/services/vaultwarden.nix
+++ modules/server/services/vaultwarden.nix
@@ -8,7 +8,6 @@
       SIGNUPS_ALLOWED = false;
       ROCKET_ADDRESS = "127.0.0.1";
       ROCKET_PORT = 8222;
-      DATABASE_URL = "sqlite:///srv/ssd-buffer/services/vaultwarden/db.sqlite3";
       WEBSOCKET_ENABLED = true;
     };
     backupDir = "/srv/ssd-buffer/services/vaultwarden/backup";
