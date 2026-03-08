b8247796d7ca83a607419ac13b691856c2ba455a
Polly <p.jonasch@posteo.de>
2026-03-08 14:49:26 +0100

    ZFS forceImportAll on boot2

--- hosts/homeserver/default.nix
+++ hosts/homeserver/default.nix
@@ -19,8 +19,6 @@
   systemd.targets.hibernate.enable = false;
   systemd.targets.hybrid-sleep.enable = false;
 
-  ];
-
   # ── Bootloader ────────────────────────────────────────────
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
