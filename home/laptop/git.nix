64fb44b9756a7fe9b1101ff990b308627f9f28de
Philip <philip@home.lan>
2026-03-10 21:45:45 +0100

    feat: Home-Manager für Laptop aktivieren (home/laptop/)

---
+++ home/laptop/git.nix
@@ -0,0 +1,23 @@
+{ ... }:
+{
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Philip";
+        email = "philip@home.lan";
+      };
+      init.defaultBranch = "main";
+      pull.rebase = true;
+      push.autoSetupRemote = true;
+      core.editor = "nano";
+    };
+    ignores = [
+      ".DS_Store"
+      "*.swp"
+      "*~"
+      ".direnv"
+      "result"
+    ];
+  };
+}
