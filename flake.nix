fb8861b9a6ca323677282c735cee771511006cda
Philip <philip@home.lan>
2026-03-11 21:52:52 +0100

    feat: flake-parts + import-tree Infrastruktur (Phase C-1)

--- flake.nix
+++ flake.nix
@@ -4,6 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -25,52 +28,8 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, sops-nix, disko, autoaspm, ... }:
-  {
-    # ── Maschine 1: Homeserver (ASRock N100DC-ITX) ──────────
-    nixosConfigurations.homeserver = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/homeserver
-        ./modules/common
-        ./modules/server
-
-        disko.nixosModules.disko
-        sops-nix.nixosModules.sops
-        autoaspm.nixosModules.default
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.philip = import ./home/server;
-          };
-        }
-      ];
-    };
-
-    # ── Maschine 2: ThinkPad P15 ───────────────────────────
-    nixosConfigurations.thinkpad-p15 = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./hosts/thinkpad-p15
-        ./modules/common
-        ./modules/desktop
-
-        sops-nix.nixosModules.sops
-
-        home-manager.nixosModules.home-manager
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.polly = import ./home/laptop;
-          };
-        }
-      ];
-    };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = [ "x86_64-linux" ];
+    imports = [ (inputs.import-tree ./parts) ];
   };
 }
