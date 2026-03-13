6b71e52b58cd6b7976e457565ae351cb95c35f80
Philip <philip@home.lan>
2026-03-13 20:05:56 +0100

    fix: fail2ban multiplier → multipliers

--- hosts/vps/fail2ban.nix
+++ hosts/vps/fail2ban.nix
@@ -9,7 +9,7 @@
     bantime = "1h";
     bantime-increment = {
       enable = true;   # Wiederholungstäter: Banzeit verdoppelt sich
-      multiplier = "2";
+      multipliers = "1 2 4 8 16 32 64";
       maxtime = "168h"; # Max. 1 Woche
     };
     jails.sshd = {
